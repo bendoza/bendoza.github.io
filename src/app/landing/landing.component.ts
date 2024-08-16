a2a65ffbabdbf121cf2582108a31b0ecb5169598
bendoza <b.mendoza@ufl.edu>
2024-08-16 14:25:20 -0400

    init firebase hosting, extended tailwind for landing on desktop lg+ displays, continued designing landing on lg+ only needing to format text content

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,18 +1,49 @@
-import { Component } from '@angular/core';
-import { ResumeComponent } from '../resume/resume.component';
+import { Component, ElementRef, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ResumeComponent } from '../resume/resume.component';
 
 @Component({
   selector: 'app-landing',
-  standalone: true,
-  imports: [ResumeComponent, CommonModule],
   templateUrl: './landing.component.html',
-  styleUrl: './landing.component.css'
+  imports: [CommonModule, ResumeComponent],
+  standalone: true,
+  styleUrls: ['./landing.component.css']
 })
-export class LandingComponent {
+export class LandingComponent implements AfterViewInit {
 
   darkMode = true;
   rotation = 0;
+  private resetDuration = 300;
+
+  blocks = Array(2000).fill(0);
+
+  constructor(private elRef: ElementRef) {}
+
+  ngAfterViewInit() {
+    
+    const blocks = Array.from(this.elRef.nativeElement.querySelectorAll(".block")) as HTMLElement[];
+
+    blocks.forEach((block) => {
+      let timeoutId: number;
+
+      block.addEventListener("mouseover", () => {
+        clearTimeout(timeoutId);
+        block.classList.add("active");
+
+        // Remove the "active" class after some time
+        timeoutId = window.setTimeout(() => {
+          block.classList.remove("active");
+        }, this.resetDuration);
+      });
+
+      block.addEventListener("mouseout", () => {
+        timeoutId = window.setTimeout(() => {
+          block.classList.remove("active");
+        }, this.resetDuration);
+      });
+    });
+
+  }
 
   toggleDarkMode() {
     this.rotation += 360;
--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,26 @@ module.exports = {
               },
             spacing: {
                 '1/10': '10%',
-            }
+                '1/9': '11.1%',
+                '1/6': '16.7%',
+                '3/5': '60%',
+                '1/4': '25%',
+                '1/5': '20%',
+                '11/20': '55%',
+                '4/5': '80%',
+                '7/10': '70%',
+                '2/5': '40%',
+                '3/10': '30%'
+            },
+            keyframes: {
+                scrollUp: {
+                  '0%': { transform: 'translateY(0)', opacity: '1' },
+                  '100%': { transform: 'translateY(-100%)', opacity: '0' },
+                },
+              },
+              animation: {
+                'scroll-up': 'scrollUp 0.5s ease-in-out forwards',
+              },
         },
     },
     plugins: [],
